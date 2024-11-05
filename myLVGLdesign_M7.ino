f420c6b9181d0eefbbc72c5981b1b34869b96438
Tom Storebø <tomstoreboe@gmail.com>
2024-11-05 22:49:15 +0000

    no malloc and no combined struct

--- myLVGLdesign_M7.ino
+++ myLVGLdesign_M7.ino
@@ -91,6 +91,7 @@ typedef struct user_data_t {
   const char* label_unit;
 };
 
+<<<<<<< Updated upstream
 SensorData sensorData;
 CanData canData;
 user_data_t userData;
@@ -102,11 +103,18 @@ struct CombinedData {
 };
 
 CombinedData combinedData;
+=======
+// declare global instances
+static SensorData sensorData;
+static CanData canData;
+static user_data_t userData;
+>>>>>>> Stashed changes
 
 // Variables
 //unsigned long delay = 10000; // duration water heater stays on (ms)
 
 
+<<<<<<< Updated upstream
 // CREATE SWITCH AND DISABLE ON CONDITIONS //////////////////////////////////////////////
 void create_switch(lv_obj_t *parent, const char *label_text, uint8_t relay_pin, lv_coord_t y_offset, uint8_t dcl_limit, unsigned long timeout_ms = 0, uint8_t sensor1_pin = 0, uint8_t sensor2_pin = 0) {
   CombinedData * data = (CombinedData *)malloc(sizeof(CombinedData));
@@ -117,27 +125,46 @@ void create_switch(lv_obj_t *parent, const char *label_text, uint8_t relay_pin, 
   data->userData.timeout_ms = timeout_ms;
   data->userData.sensor1_pin = sensor1_pin;
   data->userData.sensor2_pin = sensor2_pin;
+=======
+// CREATE BUTTON INSTANCE
+void create_button(lv_obj_t *parent, const char *label_text, uint8_t relay_pin, lv_coord_t y_offset, uint8_t dcl_limit, unsigned long timeout_ms = 0, uint8_t sensor1_pin = 0, uint8_t sensor2_pin = 0) {
+  // Allocate memory for every switch's new instance
+  //CombinedData * data = (CombinedData *)malloc(sizeof(CombinedData));
+  // Update user data with the instances passed arguments
+  userData.relay_pin = relay_pin;
+  userData.y_offset = y_offset;
+  userData.dcl_limit = dcl_limit;
+  userData.timeout_ms = timeout_ms;
+  userData.sensor1_pin = sensor1_pin;
+  userData.sensor2_pin = sensor2_pin;
+>>>>>>> Stashed changes
 
   // create button object and add to struct
-  data->userData.my_btn = lv_btn_create(parent); // IMPORTANT TO STORE BUTTON IN USERDATA STRUCT - ELSE CLEARING BUTTONS WON'T WORK
-  lv_obj_set_pos(data->userData.my_btn, 10, y_offset);
-  lv_obj_t *label = lv_label_create(data->userData.my_btn);
+  userData.my_btn = lv_btn_create(parent); // IMPORTANT TO STORE BUTTON IN USERDATA STRUCT - ELSE CLEARING BUTTONS WON'T WORK
+  lv_obj_set_pos(userData.my_btn, 10, y_offset);
+  lv_obj_t *label = lv_label_create(userData.my_btn);
   lv_label_set_text(label, label_text);
   lv_obj_center(label);
-  lv_obj_add_flag(data->userData.my_btn, LV_OBJ_FLAG_CHECKABLE);
+  lv_obj_add_flag(userData.my_btn, LV_OBJ_FLAG_CHECKABLE);
 
   // create temperature dropdown and dynamic temperature labels
   if ( ! timeout_ms ) {
-    create_temperature_dropdown(parent, data);
+    create_temperature_dropdown(parent);
     // create timed labels
+<<<<<<< Updated upstream
     data->userData.label_obj = lv_label_create(lv_obj_get_parent(data->userData.my_btn));
     lv_timer_t *timer = lv_timer_create(display_temp, 10000, data);
     lv_obj_set_pos(data->userData.label_obj, 180, y_offset + 13);
+=======
+    userData.label_obj = lv_label_create(lv_obj_get_parent(userData.my_btn));
+    lv_timer_create(display_temp, 10000, NULL);
+    lv_obj_set_pos(userData.label_obj, 180, y_offset + 13);
+>>>>>>> Stashed changes
   }
   
   // lets set label text
-  if ( data->canData.dcl < dcl_limit ) {
-    lv_obj_add_state(data->userData.my_btn, LV_STATE_DISABLED);
+  if ( canData.dcl < dcl_limit ) {
+    lv_obj_add_state(userData.my_btn, LV_STATE_DISABLED);
     lv_obj_t *label2low = lv_label_create(parent);
     lv_label_set_long_mode(label2low, LV_LABEL_LONG_SCROLL_CIRCULAR);
     lv_label_set_text(label2low, "Please Charge Battery                                            ");
@@ -154,55 +181,59 @@ void create_switch(lv_obj_t *parent, const char *label_text, uint8_t relay_pin, 
   // create event handlers with custom user_data
   // CLICKED events for timer and ALL events for thermostat
   if ( timeout_ms ) {
-    lv_obj_add_event_cb(data->userData.my_btn, timer_event_handler, LV_EVENT_ALL, data);
+    lv_obj_add_event_cb(userData.my_btn, timer_event_handler, LV_EVENT_ALL, NULL);
   }
   else {
-    lv_obj_add_event_cb(data->userData.my_btn, thermostat_event_handler, LV_EVENT_ALL, data);
+    lv_obj_add_event_cb(userData.my_btn, thermostat_event_handler, LV_EVENT_ALL, NULL);
   }
 }
 
 // timer event handler function - HOT WATER ///////////////////////////////////////////////////////////
 static void timer_event_handler(lv_event_t * e) {
-  CombinedData * data = (CombinedData *)lv_event_get_user_data(e);
+  //CombinedData * data = (CombinedData *)lv_event_get_user_data(e);
   lv_event_code_t code = lv_event_get_code(e);
   lv_obj_t * obj = lv_event_get_target(e);
-  uint8_t relay_pin = data->userData.relay_pin;
+  //uint8_t relay_pin = userData.relay_pin;
     if(code == LV_EVENT_CLICKED) {
     LV_UNUSED(obj);
-    if ( lv_obj_has_state(data->userData.my_btn, LV_STATE_CHECKED) ) {
-      digitalWrite(relay_pin, HIGH);
+    if ( lv_obj_has_state(userData.my_btn, LV_STATE_CHECKED) ) {
+      digitalWrite(userData.relay_pin, HIGH);
       // Start timer
+<<<<<<< Updated upstream
       lv_timer_t * timer = lv_timer_create(switch_off, 10000, data); // want to disable timer once excess solar to hot water is activated
+=======
+      lv_timer_create(switch_off, hot_water_timer, NULL); // want to disable timer once excess solar to hot water is activated
+>>>>>>> Stashed changes
     }
     else {
-      digitalWrite(relay_pin, LOW);
+      digitalWrite(userData.relay_pin, LOW);
     }
   }
 }
 // SWITCH OFF CALLBACK FUNCTION ///////////////////////////////////////////////////////////////////////
 void switch_off(lv_timer_t * timer) {
-  CombinedData * data = (CombinedData *)timer->user_data;
+  //CombinedData * data = (CombinedData *)timer->user_data;
   
   // turn off the relay
-  digitalWrite(data->userData.relay_pin, LOW);
+  digitalWrite(userData.relay_pin, LOW);
   
   // clear button flag
-  lv_obj_clear_state(data->userData.my_btn, LV_STATE_CHECKED);
+  lv_obj_clear_state(userData.my_btn, LV_STATE_CHECKED);
 }
 
 // thermostat event handler function - HEATERS /////////////////////////////////////////////////////////
 void thermostat_event_handler(lv_event_t * e) {
-  CombinedData * data = (CombinedData *)lv_event_get_user_data(e);
+  //CombinedData * data = (CombinedData *)lv_event_get_user_data(e);
   lv_event_code_t code = lv_event_get_code(e);
   lv_obj_t * obj = lv_event_get_target(e);
   
   if(code == LV_EVENT_CLICKED) {
     LV_UNUSED(obj);
-    if ( lv_obj_has_state(data->userData.my_btn, LV_STATE_CHECKED) ) {
-      lv_timer_t * timer = lv_timer_create(thermostat_timer, 10000, data); // check temp diff every 10s
+    if ( lv_obj_has_state(userData.my_btn, LV_STATE_CHECKED) ) {
+      lv_timer_create(thermostat_timer, 10000, NULL); // check temp diff every 10s
     }
     else {
-      digitalWrite(data->userData.relay_pin, LOW);
+      digitalWrite(userData.relay_pin, LOW);
       //lv_timer_set_repeat_count(timer, 1);
     }
   }
@@ -211,8 +242,8 @@ void thermostat_event_handler(lv_event_t * e) {
 ////////////////////////////////////////////////////////// DROP DOWN ///////////////////////////////////////
 // event handler for temperature dropdown
 void dropdown_event_handler(lv_event_t *e) {
-    CombinedData * data = (CombinedData *)lv_event_get_user_data(e);
-    if ( !data && !data->userData.my_btn ) {
+    //CombinedData * data = (CombinedData *)lv_event_get_user_data(e);
+    if ( !userData.my_btn ) {
       Serial.println("dropdown_event_handler: no user data");
       return;
     }
@@ -222,82 +253,90 @@ void dropdown_event_handler(lv_event_t *e) {
     // link index to selection
     switch (id_selected) {
       case 0:
-        data->userData.set_temp = 5;
+        userData.set_temp = 5;
         break;
       case 1:
-        data->userData.set_temp = 18;
+        userData.set_temp = 18;
         break;
       case 2:
-        data->userData.set_temp = 19;
+        userData.set_temp = 19;
         break;
       case 3:
-        data->userData.set_temp = 20;
+        userData.set_temp = 20;
         break;
       case 4:
-        data->userData.set_temp = 21;
+        userData.set_temp = 21;
         break;
       case 5:
-        data->userData.set_temp = 22;
+        userData.set_temp = 22;
         break;
       default:
-        data->userData.set_temp = 23;
+        userData.set_temp = 23;
     }
 }
 
 // CREATE TEMPERATURE SELECTION DROPDOWN MENU ///////////////////////////////////////
-void create_temperature_dropdown(lv_obj_t * parent, CombinedData *data) {
+void create_temperature_dropdown(lv_obj_t * parent) {
   lv_obj_t *dd = lv_dropdown_create(parent);
-  uint8_t y_offset = data->userData.y_offset;
+  //uint8_t y_offset = userData.y_offset;
   lv_dropdown_set_options(dd,
     "5\u00B0C\n18\u00B0C\n19\u00B0C\n20\u00B0C\n21\u00B0C\n22\u00B0C\n23\u00B0C");
     
   // set user data
   lv_dropdown_set_selected(dd, 3); // default index to be displayed. value set_temp in struct
-  lv_obj_set_user_data(dd, data);
-  lv_obj_add_event_cb(dd, dropdown_event_handler, LV_EVENT_VALUE_CHANGED, data);
+  lv_obj_set_user_data(dd, NULL);
+  lv_obj_add_event_cb(dd, dropdown_event_handler, LV_EVENT_VALUE_CHANGED, NULL);
   
   // place roller
-  lv_obj_set_pos(dd, 255, y_offset - 1);
+  lv_obj_set_pos(dd, 255, userData.y_offset - 1);
   lv_obj_set_width(dd, 80);
 }
 
 // THERMOSTAT TIMER AND DISPLAY FUNCTION /////////////////////////////////////////////////////////////////////
 void thermostat_timer(lv_timer_t * timer) {
-  CombinedData * data = (CombinedData *)timer->user_data;
+  //CombinedData * data = (CombinedData *)timer->user_data;
   
-  if ( data->userData.sensor2_pin ) { // use avg temp
+  if ( userData.sensor2_pin ) { // use avg temp
     // Close relay if temperature is below selected and button has been pressed
-    if ( data->sensorData.avg_temp < data->userData.set_temp && lv_obj_has_state(data->userData.my_btn, LV_STATE_CHECKED) ) {
-    digitalWrite(data->userData.relay_pin, HIGH); 
+    if ( sensorData.avg_temp < userData.set_temp && lv_obj_has_state(userData.my_btn, LV_STATE_CHECKED) ) {
+    digitalWrite(userData.relay_pin, HIGH); 
     }
     // Open relay when temperature is higher or equal to selected
     else {
-      digitalWrite(data->userData.relay_pin, LOW);
+      digitalWrite(userData.relay_pin, LOW);
     }
   }
   else {
     // Close relay if temperature is below selected and button has been pressed
-    if ( data->sensorData.temp3 < data->userData.set_temp && lv_obj_has_state(data->userData.my_btn, LV_STATE_CHECKED) ) { // temp3 set as it is easier to code for now
-      digitalWrite(data->userData.relay_pin, HIGH); 
+    if ( sensorData.temp3 < userData.set_temp && lv_obj_has_state(userData.my_btn, LV_STATE_CHECKED) ) { // temp3 set as it is easier to code for now
+      digitalWrite(userData.relay_pin, HIGH); 
     }
     // Open relay when temperature is higher or equal to selected
     else {
-      digitalWrite(data->userData.relay_pin, LOW);
+      digitalWrite(userData.relay_pin, LOW);
     }
   }  
 }
 
 // DISPLAY TEMP FUNCTION ///////////////////////////////////////////////////////////////
 void display_temp(lv_timer_t *timer) {
-  CombinedData * data = (CombinedData *)timer->user_data;
+  //CombinedData * data = (CombinedData *)timer->user_data;
   char buf[10];
+<<<<<<< Updated upstream
   if ( data->userData.sensor2_pin ) {
     snprintf(buf, sizeof(buf), "%.1f\u00B0C", data->sensorData.avg_temp);
   }
   else {
     snprintf(buf, sizeof(buf), "%.1f\u00B0C", data->sensorData.temp4); // SENSOR 4 SELECTED FOR TESTING ///////// data-> doesn't work
+=======
+  if ( userData.sensor2_pin ) {
+    snprintf(buf, sizeof(buf), "%.1f\u00B0C", sensorData.avg_temp); // use the global shared instance for temp and humidity
   }
-  lv_label_set_text(data->userData.label_obj, buf);
+  else {
+    snprintf(buf, sizeof(buf), "%.1f\u00B0C", sensorData.temp3);
+>>>>>>> Stashed changes
+  }
+  lv_label_set_text(userData.label_obj, buf);
 }
 
 // CLEAR CAN EVENT HANDLER ////////////////////////////////////////////////////////////////
@@ -311,17 +350,27 @@ void clear_bms_fault(lv_event_t * e) {
   }
 }
 // REFRESH CAN LABEL DATA //////////////////////////////////////////////////////////////////////
+<<<<<<< Updated upstream
 void refresh_data(lv_timer_t* timer) {
     CombinedData *data = (CombinedData *)timer->user_data;
     char buf[50];
     
     // Determine the type of property and format accordingly
     snprintf(buf, sizeof(buf), "%s %d %s", data->userData.label_prefix, *(int*)(data->userData.canDataProperty),data->userData.label_unit); // try without data->
+=======
+void refresh_can_data(lv_timer_t* timer) {
+    //CombinedData *data = (CombinedData *)timer->user_data;
+    char buf[50];
+    
+    // Determine the type of property and format accordingly
+    snprintf(buf, sizeof(buf), "%s %d %s", userData.label_prefix, *(int*)(userData.canDataProperty),userData.label_unit);
+>>>>>>> Stashed changes
 
-    lv_label_set_text(data->userData.label_obj, buf);
+    lv_label_set_text(userData.label_obj, buf);
 }
 
 void create_can_label(lv_obj_t* parent, const char* label_prefix, const char* label_unit, void* canDataProperty, int x_pos, int y_pos) {
+<<<<<<< Updated upstream
     CombinedData *data = (CombinedData *)parent->user_data;
     data->userData.label_obj = lv_label_create(parent);
     data->userData.canDataProperty = canDataProperty;
@@ -331,10 +380,27 @@ void create_can_label(lv_obj_t* parent, const char* label_prefix, const char* la
     lv_obj_set_pos(data->userData.label_obj, x_pos, y_pos);
     lv_timer_t* timer = lv_timer_create(refresh_data, 200, data);
 } 
+=======
+    // Allocate memory for new user data instance
+    //CombinedData * data = (CombinedData *)malloc(sizeof(CombinedData));
+    //if (data) {
+        // Update instance with user data
+        userData.label_obj = lv_label_create(parent);
+        userData.canDataProperty = canDataProperty;
+        userData.label_prefix = label_prefix;
+        userData.label_unit = label_unit;
+
+        lv_obj_set_pos(userData.label_obj, x_pos, y_pos);
+        lv_timer_create(refresh_can_data, 200, NULL);
+    //}
+}
+
+>>>>>>> Stashed changes
   
 // RETRIEVE DATA FROM M4 CORE
 void retrieve_M4_data() {
     // Call the RPC function to get sensor data
+<<<<<<< Updated upstream
     if ( RPC.available() ) {
 <<<<<<< Updated upstream
       auto sensorResponse = RPC.call("getSensorData").as<SensorData>();
@@ -352,6 +418,10 @@ void retrieve_M4_data() {
 >>>>>>> Stashed changes
     }
     else Serial.println("Waiting for RPC to become available");
+=======
+      sensorData = RPC.call("getSensorData").as<SensorData>();
+      //canData = RPC.call("getCanData").as<CanData>(); // until can issue on m4 is solved marked out as it causes crash
+>>>>>>> Stashed changes
 }
 
 // CREATE NON CLICK ARC //////////////////////////////////////////////////////////////////////////
@@ -438,10 +508,17 @@ void setup() {
                         LV_GRID_ALIGN_STRETCH, 0, 1);
 
   // Create labels for CAN data
+<<<<<<< Updated upstream
   /*create_can_label(cont, "SOC", "\u0025", &combinedData.canData.soc, 20, 20);
   create_can_label(cont, "Amperage", "A", &combinedData.canData.rawI, 200, 20);
   create_can_label(cont, "Voltage", "V", &combinedData.canData.rawU, 20, 50);
   create_can_label(cont, "Power", "W", &combinedData.canData.p, 200, 50);*/
+=======
+  create_can_label(cont, "SOC", "\u0025", &canData.soc, 20, 20);
+  create_can_label(cont, "Amperage", "A", &canData.rawI, 200, 20);
+  create_can_label(cont, "Voltage", "V", &canData.rawU, 20, 50);
+  create_can_label(cont, "Power", "W", &canData.p, 200, 50);
+>>>>>>> Stashed changes
     
   // Create button to clear faults
   /*lv_obj_t* bms_btn = lv_btn_create(cont);
@@ -459,13 +536,13 @@ void setup() {
                             LV_GRID_ALIGN_STRETCH, 0, 1);
 
   // Create Switch 1 - args: obj, label, relay_pin, y_offset, dcl_limit, timeout_ms, (sensor1_pin), (sensor2_pin)
-  create_switch(cont, "Ceiling Heater", RELAY1, 20, 70, 0, DHTPIN1, DHTPIN2);
+  create_button(cont, "Ceiling Heater", RELAY1, 20, 70, 0, DHTPIN1, DHTPIN2);
 
   // Create Switch 2
-  create_switch(cont, "Shower Heater", RELAY2, 120, 10, 0, DHTPIN3);
+  create_button(cont, "Shower Heater", RELAY2, 120, 10, 0, DHTPIN3);
 
   // Create Switch 3
-  create_switch(cont, "Hot Water", RELAY3, 300, 60, 10000);
+  create_button(cont, "Hot Water", RELAY3, 300, 60, 10000);
 }
 
 // LOOP ///////////////////////////////////////////////////////////////////////////////////
@@ -489,6 +566,7 @@ void loop() {
   if (buffer.length() > 0) {
     Serial.print(buffer);
 
+<<<<<<< Updated upstream
     Serial.print("SOC: ");
     Serial.println(combinedData.canData.soc);
     /*Serial.print("Current: ");
@@ -496,5 +574,14 @@ void loop() {
     Serial.print("Temperature and Humidity: ");
     Serial.println(combinedData.sensorData.temp4);
     Serial.println(combinedData.sensorData.humi4);*/
+=======
+  //  Serial.print("M7 SOC: ");
+//    Serial.println(canData.soc);
+    //Serial.print("M7 Current: ");
+    //Serial.println(canData.rawI);
+    Serial.print("M7 Temperature and Humidity: ");
+    Serial.println(sensorData.temp3);
+    Serial.println(sensorData.humi3);
+>>>>>>> Stashed changes
   }
 }
