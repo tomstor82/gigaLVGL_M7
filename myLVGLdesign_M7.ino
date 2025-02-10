f3c6aa648b40dfcec8b02df1b82339e89fafc995
Tom Storebø <tomstoreboe@gmail.com>
2025-02-10 20:19:12 +0000

    Working: Reverted power_check back to return instead of lv_timer_reset as it continues function but restarts timer. fixed loop inverter start issue with int time_ms = millis() causing issues. updated e_handler inverter to check 0x01 instead of 0x02

--- myLVGLdesign_M7.ino
+++ myLVGLdesign_M7.ino
@@ -321,24 +321,25 @@ void create_button(lv_obj_t *parent, const char *label_text, uint8_t relay_pin, 
 void mppt_delayer(bool mppt_delay) {
   static uint32_t start_time_ms = 0;
 
-  // IF TRUE ARGUMENT: START TIMER TO ALLOW MPPT TO REMAIN OFF FOR AT LEAST 20s
+  // START TIMER WHEN DELAY STARTS
   if ( mppt_delay && ! start_time_ms ) {
     start_time_ms = millis(); // Start the timer
   }
 
-  // IF FALSE ARGUMENT AND TIMER HAS NOT REACHED 20s: SET TRUE
-  else if ( ! mppt_delay && start_time_ms && start_time_ms + 20000 > millis() ) {
+  // CHECK THAT MPPT DELAY LAST FOR AT LEAST 20s TO AVOID FLAPPING CONDITION
+  else if ( ! mppt_delay && start_time_ms && 20000 + start_time_ms > millis() ) {
     mppt_delay = true;
   }
   else {
     start_time_ms = 0; // Reset the timer after 20 seconds
   }
 
-  // Update CAN_MSG and CAN_TX based on mppt_delay
+  // SET CAN MESSAGE AND ENABLE TX
   if ( mppt_delay ) {
     CAN_MSG[1] = 0x01;
     CAN_TX_MPO1 = true; // This triggers send function in loop
   }
+  // REMOVE MESSAGE AND DISABLE TX IF CCL IS NOT ENFORCED
   else if ( ! CCL_ENFORCED ) {
     CAN_TX_MPO1 = false;
     CAN_MSG[1] = 0x00;
@@ -346,7 +347,7 @@ void mppt_delayer(bool mppt_delay) {
 }
 
 
-// SUNRISE DETECTOR - ONLY WHEN INVERTER IS OFF //////////////////////////////////////////////////////
+// SUNRISE DETECTOR - ACTIVE WHEN INVERTER IS OFF //////////////////////////////////////////////////////
 void sunrise_detector() {
 
   static bool mppt_delay = false;
@@ -690,9 +691,9 @@ void power_check(lv_timer_t *timer) {
   bool on = false;
 
   // SLEEP MODE VARIABLES
-  uint32_t time_ms = 0;
-  uint8_t minute_count = 0;
-  bool pre_sleep_delay = false;
+  static uint32_t time_ms = 0;
+  static uint8_t minute_count = 0;
+  static bool pre_sleep_delay = false;
   char plural[2] = "s";
   char label[30];
 
@@ -734,7 +735,7 @@ void power_check(lv_timer_t *timer) {
         time_ms = 0; // RESET SLEEP TIMER
       }
     }
-    lv_timer_reset(data->timer);
+    return;
   }
 
   // INVERTER SLEEP MODE
@@ -744,10 +745,10 @@ void power_check(lv_timer_t *timer) {
     if ( ! time_ms ) {
       time_ms = millis();
       pre_sleep_delay = true;
-      lv_timer_reset(data->timer); // to prevent label being written once finished
+      return; // to prevent label being written once finished
     }
-    // KEEP INVERTER ON FOR AT LEAST 30s + POWER_CHECK TIMER
-    else if ( (time_ms + 30 * 1000) < millis() && pre_sleep_delay ) {
+    // KEEP INVERTER ON FOR AT LEAST 40s + POWER_CHECK TIMER = 1 min
+    else if ( (time_ms + 40 * 1000) < millis() && pre_sleep_delay ) {
       time_ms = millis();
       digitalWrite(data->relay_pin, LOW);
       pre_sleep_delay = false;
@@ -763,14 +764,14 @@ void power_check(lv_timer_t *timer) {
       time_ms = 0;
       data->on = false; // to enable inverter startup check
       lv_event_send(data->button, LV_EVENT_CLICKED, NULL);
-      lv_timer_reset(data->timer); // to prevent label being written once finished
+      return; // to prevent label being written once finished
     }
 
     if ( ! pre_sleep_delay ) {
       snprintf(label, sizeof(label), "OFF - NO LOAD\nON in %d minute%s", (off_interval_min - minute_count), plural);
       lv_label_set_text(data->label_obj, label);
     }
-    lv_timer_reset(data->timer);
+    return;
   }
 
   // HOT WATER OFF
@@ -796,7 +797,7 @@ void hot_water_inverter_event_handler(lv_event_t *e) {
       if ( data->relay_pin == RELAY1 ) {
         inverter_prestart_p = WATTS;
         // TURN OFF MPPT IF SOLAR DETECTED BUT NO CHARGE TO AVOID START-UP POWER SURGE
-        if ( WATTS >= 0 && (CUSTOM_FLAGS & 0x02) == 0x02 ) {
+        if ( WATTS >= 0 && (CUSTOM_FLAGS & 0x01) == 0x01 ) {
           mppt_delayer(true);
           strcpy(DYNAMIC_LABEL, "Solar OFF - Inverter starting");
           inverter_delay = true;
@@ -836,7 +837,6 @@ void hot_water_inverter_event_handler(lv_event_t *e) {
 
       // CREATE COMBINED TIMER THAT ONLY RUNS ONCE AND IS RESET IF NEEDED INSIDE power_check
       data->timer = lv_timer_create(power_check, data->timeout_ms, data);
-      lv_timer_set_repeat_count(data->timer, 1);
 
       // SET BUTTON TO ON
       data->on = true;
@@ -1888,8 +1888,8 @@ void setup() {
   }
 
   if ( ! CAN.begin(CanBitRate::BR_500k) ) {
-        Serial.println("CAN.begin(...) failed.");
-        for (;;) {}
+    Serial.println("CAN.begin(...) failed.");
+    for (;;) {}
   }
   
   // Initialise display, touch and backlight
@@ -2003,19 +2003,28 @@ void loop() {
     }
   }
 
-  // if no touch within timeout dim display and brightness above 0
+  // IF NO TOUCH WITHIN TIMEOUT AND IF BRIGHTNESS IS ABOVE 0, DIM DISPLAY
   if ( previous_touch_ms + touch_timeout_ms < millis() && brightness ) {
     dim_display();
   }
 
-  // 2 sec inverter delay after mppt disabled
-  if ( inverter_delay ) {
-    static uint32_t time_ms = millis();
-    if ( time_ms + 2000 < millis() ) {
-      digitalWrite(userData[3].relay_pin, HIGH);
-      time_ms = 0;
+  // 2s INVERTER DELAY AFTER MPPT DISABLED WITH 20s PAUSE BEFORE SENDING RE-ENABLE SIGNAL
+  if (inverter_delay) {
+    static uint32_t time_ms = 0;
+
+    if (time_ms == 0) {
+      time_ms = millis();
+    }
+
+    // WAIT 20s BEFORE SENDING MPPT RESTART SIGNAL AS SUNRISE_DETECTOR IS DISABLED WITH INVERTER ON
+    if (20000 + time_ms < millis()) {
       inverter_delay = false;
       mppt_delayer(false);
+      time_ms = 0;
+    }
+    // ALLOW MPPT 2s TO LOOSE POWER TO AVOID POWER SURGE
+    else if (2000 + time_ms < millis()) {
+      digitalWrite(userData[3].relay_pin, HIGH);
     }
   }
   /*if (Serial) {
